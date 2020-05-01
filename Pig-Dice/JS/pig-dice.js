6d666042c9982d9d3432aca4b3d9687c5d82f6fa
jacobpineault <jacob.pineault5@gmail.com>
2020-05-01 12:03:47 -0400

    games played update

--- Pig-Dice/JS/pig-dice.js
+++ Pig-Dice/JS/pig-dice.js
@@ -5,15 +5,22 @@ const dieRoll = () => {
 
 const setScore = () => {
   let dieCtrl = playPigDice();
-  document.getElementById("diceBox").value = dieCtrl;
+  document.getElementById("diceBox").value = `Score: ${dieCtrl}`;
 };
 
 const playPigDice = () => {
   let score = 0;
+  let gamesPlayed = 0;
   let die = dieRoll();
   while (die != 1) {
+    gamesPlayed++;
     score += die;
     die = dieRoll();
   }
+  highScore(gamesPlayed);
   return score;
+};
+
+const highScore = (gamesPlayed) => {
+  document.getElementById("highscore").value = `Total games: ${gamesPlayed}`;
 };
