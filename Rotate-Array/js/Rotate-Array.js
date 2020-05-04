54c64165d7f1a1eb77871ec2b6a0a80b55326f6a
Jacob Pineault <jacob.pineault5@gmail.com>
2020-05-03 23:31:54 -0400

    update

--- Rotate-Array/js/Rotate-Array.js
+++ Rotate-Array/js/Rotate-Array.js
@@ -1,4 +1,21 @@
-let genArray = new Array(5);
+let generatedArray = new Array();
+let newArray = new Array();
+let shiftedArray = new Array();
+let totalRotations = 0;
+
+class ArraysCollection {
+  value;
+
+  print() {
+    console.log(this.value);
+  }
+
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+let items = [];
 
 const generateRandom = () => {
   let randNbr = Math.floor(Math.random() * 100) + 1;
@@ -6,13 +23,48 @@ const generateRandom = () => {
 };
 
 const fillArray = () => {
-  let size = genArray.length;
-  while (size < 5) {
+  for (let size = 0; size < 3; size++) {
     let nbr = generateRandom();
-    genArray.push(nbr);
+    generatedArray.push(nbr);
   }
+  let newArray = new ArraysCollection(generatedArray);
+  items.push(newArray);
+  display(newArray);
 };
 
-const rotateRight = (genArray) => {};
+const display = (array) => {
+  let tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
+  let tr = `<tr><td>${array.toString}</td></tr>`;
+  tbody.innerHTML += tr;
+  // for (let item of items) {
+  //   let tr = `<tr><td>${item.value}</td></tr>`;
+  //   tbody.innerHTML += tr;
+  // }
+};
 
-const rotateLeft = (genArray) => {};
+const reset = () => {
+  generatedArray = [];
+  newArray = [];
+  items = [];
+  tbody.innerHTML = "";
+};
+
+const rotateRight = (genArray) => {
+  let tempHold1;
+  tempHold1 = newArray[0];
+  for (let idx = 1; idx < newArray.length; idx++) {
+    shiftedArray.push(newArray[idx]);
+    console.log(idx);
+  }
+  shiftedArray.push(tempHold1);
+  // genArray = genArray.shift();
+  // genArray.push(tempHold1);
+  // newestArray.push(genArray);
+  totalRotations++;
+  display(shiftedArray);
+};
+
+const rotateLeft = (genArray) => {
+  totalRotations++;
+};
