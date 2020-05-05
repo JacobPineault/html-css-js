85a37271b06a55e6e3595bdd14fb8f21c109d655
jacobpineault <jacob.pineault5@gmail.com>
2020-05-05 10:32:54 -0400

    full functioning update

--- prs/js/user-edit2.js
+++ prs/js/user-edit2.js
@@ -3,9 +3,12 @@ let url = "http://localhost:8080";
 let user = null;
 
 $().ready(() => {
-  console.log("jQuery is ready");
+  // console.log("jQuery is ready");
   $("#get").click(() => {
     getUser();
+  });
+  $("#save").click(() => {
+    updateUser();
   });
 });
 
@@ -35,4 +38,33 @@ const display = () => {
   $("#eemail").val(user.email);
   $("#eisreviewer").prop("checked", user.reviewer);
   $("#eisadmin").prop("checked", user.admin);
+};
+
+const updateUser = () => {
+  user.userName = $("#eusername").val();
+  user.password = $("#epassword").val();
+  user.firstName = $("#efirstname").val();
+  user.lastName = $("#elastname").val();
+  user.phoneNumber = $("#ephonenumber").val();
+  user.email = $("#eemail").val();
+  user.reviewer = $("#eisreviewer").prop("checked");
+  user.admin = $("#eisadmin").prop("checked");
+  console.debug("User before update:", user);
+  putUser(user);
+};
+
+const putUser = (user) => {
+  $.ajax({
+    method: "PUT",
+    // url: `${url}/users/${user.id}` check w/ Sean if we need ID
+    url: `${url}/users/`,
+    data: JSON.stringify(user),
+    contentType: "application/json",
+  })
+    .done((res) => {
+      console.log("Update successful", res);
+    })
+    .fail((err) => {
+      console.log("ERROR: ", err);
+    });
 };
